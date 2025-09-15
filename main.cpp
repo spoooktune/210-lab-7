0140c93e2cb4962a368737e833b34fc7b24e1d30
spoooktune <asriprasana5@gmail.com>
2025-09-14 21:15:51 -0700

    adjusted main(), reverseArray()

--- main.cpp
+++ main.cpp
@@ -3,33 +3,37 @@
 
 using namespace std;
 
-string* reverseArray(string inputArr[]){
+string *reverseArray(string inputArr[], const int SIZE){
     string temp = "";
     string *arrPtr = nullptr;
-    int size = sizeof(inputArr);
-    for (int i = 0; i < size/2; i++){
+    for (int i = 0; i < SIZE/2; i++){
         temp = inputArr[i];
-        inputArr[i] = inputArr[size - i];
-        inputArr[size - i] = temp;
+        inputArr[i] = inputArr[SIZE-1-i];
+        inputArr[SIZE-1-i] = temp;
+        
     }
     arrPtr = inputArr;
     return arrPtr;
 }
 
-void displayArray(string outputArr[]){
-    int size = sizeof(outputArr);
-    for (int i = 0; i < size; i++){
+void displayArray(string outputArr[], const int SIZE){
+    for (int i = 0; i < SIZE; i++){
         cout << outputArr[i] << " ";
     }
     cout << endl;
 }
 
 int main(){
+    const int SIZE = 5;
     string names[] = {"Jason", "Alice", "Mark", "Penelope", "Matthew"};
+    string reversed[SIZE];
     string *namesPtr = nullptr;
     cout << "Original Array: ";
-    displayArray(names);
-    namesPtr = reverseArray(names);
-    cout << "Reversed Array: ";
-    displayArray(namesPtr);
+    displayArray(names, SIZE);
+    namesPtr = reverseArray(names, SIZE);
+    for (int i = 0; i < SIZE; i++){
+        reversed[i] = *(namesPtr + i);
+    }
+    cout << "\nReversed Array: ";
+    displayArray(reversed, SIZE);
 }
