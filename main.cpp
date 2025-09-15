148d2a572d7b6c685cab7d264c28572209d06e24
spoooktune <asriprasana5@gmail.com>
2025-09-14 20:44:42 -0700

    added reverseArray() function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,20 @@
 #include <string>
 
 using namespace std;
+
+string* reverseArray(string inputArr[]){
+    string temp = "";
+    string *arrPtr = nullptr;
+    int size = sizeof(inputArr);
+    for (int i = 0; i < size/2; i++){
+        temp = inputArr[i];
+        inputArr[i] = inputArr[size - i];
+        inputArr[size - i] = temp;
+    }
+    arrPtr = inputArr;
+    return arrPtr;
+}
+
 int main(){
-    
+
 }
